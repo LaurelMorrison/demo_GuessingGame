6792c58193590c7dd03cd65480f947795fb56171
Laurel <lbentleym1992@gmail.com>
2021-05-25 11:25:02 -0400

    step one complete

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+System.Console.Write("Guess a secret number");
+
+string number = System.Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(number))
+{
+    System.Console.WriteLine("Fine, don't guess'!");
+}
+else
+{
+    System.Console.WriteLine($"Your guess is {number}!");
+}
