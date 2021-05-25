662cf5273bb09573ebd1b1c2f82b03faed3629ac
Laurel <lbentleym1992@gmail.com>
2021-05-25 15:01:34 -0400

    phase 7 complete

--- Program.cs
+++ Program.cs
@@ -1,49 +1,79 @@
 ﻿using System;
 
-GuessingGame();
-
-static int GuessingGame()
+class Program
 {
-    System.Console.Write("Guess a secret number between 1-100");
-    int guess = 0;
+    static void Main()
+    {
+        bool difficultyCheck = true;
+        int allowedGuesses = 0;
+        int guess = 0;
+        int randomNumber = new Random().Next(1, 100);
 
-    try
-    {
-        guess = int.Parse(Console.ReadLine());
-    }
-    catch (Exception)
-    {
-        Console.WriteLine("Invalid guess");
-        guess = GuessingGame();
-    }
-    return guess;
-}
+        while (difficultyCheck)
+        {
+            Console.Write(@"what difficulty level would you like? 
+        1. Easy
+        2. Medium
+        3. Hard
+        "
+            );
 
-int allowedGuesses = 3;
-int numGuesses = 0;
-int randomNumber = new Random().Next(1, 100);
+            int DifficultyLevel = int.Parse(Console.ReadLine());
+            if (DifficultyLevel == 1)
+            {
+                allowedGuesses = 8;
+                difficultyCheck = false;
+            }
+            else if (DifficultyLevel == 2)
+            {
+                allowedGuesses = 6;
+                difficultyCheck = false;
+            }
+            else if (DifficultyLevel == 3)
+            {
+                allowedGuesses = 4;
+                difficultyCheck = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid response, please enter easy, medium or hard");
+            };
+        }
 
-while (true)
-{
-    int guess = GuessingGame();
-    numGuesses++;
-    if (guess == randomNumber)
-    {
-        Console.WriteLine("Your guess was correct!");
-        break;
+        for (int i = 1; i <= allowedGuesses; i++)
+        {
+            Console.Write("Guess a secret number between 1-100 ");
+            try
+            {
+                guess = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid guess");
+                i--;
+                Console.Write($"You have {allowedGuesses - i} tries left. Enter another number: ");
+                continue;
+            };
+
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("Your guess was correct!");
+                break;
+            }
+            if (guess > randomNumber)
+            {
+                Console.WriteLine("Your guess was too high");
+            }
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("Your guess was too low");
+            }
+            if (allowedGuesses == i)
+            {
+                Console.WriteLine($"The number was: {randomNumber}");
+                break;
+            }
+            Console.Write($"You have {allowedGuesses - i} tries left. Enter another number: ");
+        }
     }
-    if (guess > randomNumber)
-    {
-        Console.WriteLine("Your guess was too high");
-    }
-    if (guess < randomNumber)
-    {
-        Console.WriteLine("Your guess was too low");
-    }
-    if (allowedGuesses == numGuesses)
-    {
-        Console.WriteLine($"The number was: {randomNumber}");
-        break;
-    }
-    Console.Write($"You have {allowedGuesses - numGuesses} tries left. Enter another number: ");
 }
